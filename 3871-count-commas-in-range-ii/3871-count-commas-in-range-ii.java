6e5c4a52e9a8f7dda06fcd70ab39210da4e644f9
adoreashu <147312728+adoreashu@users.noreply.github.com>
2026-09-09 11:44:42 +0530

    Time: 1 ms (99.36%), Space: 42.4 MB (85.26%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,14 +1,9 @@
 class Solution {
     public long countCommas(long n) {
-        long totalCommas = 0;
-        long threshold = 1000; 
-        
-        while (n >= threshold) {
-            totalCommas += (n - threshold + 1);
-            
-            threshold *= 1000;
-        }
-        
-        return totalCommas;
+        return Math.max(0L, n - 999L) 
+             + Math.max(0L, n - 999_999L) 
+             + Math.max(0L, n - 999_999_999L) 
+             + Math.max(0L, n - 999_999_999_999L) 
+             + Math.max(0L, n - 999_999_999_999_999L);
     }
 }
